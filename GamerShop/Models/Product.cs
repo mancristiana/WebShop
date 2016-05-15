d27f6a9cf7b68a35d0faa970b366755a85b04a7b
Cristiana Man <man.cristiana1@gmail.com>
2016-05-15 19:39:26 +0200

    Initial commit

---
+++ GamerShop/Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GamerShop.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImgUrl { get; set; }
+
+        [Required]
+        [Range(0, 99999)]
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+        
+        [Display(Name = "Left in stock")]
+        public int StockCount { get; set; }
+    }
+}
---
+++ GamerShop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GamerShop.Startup))]
+namespace GamerShop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
